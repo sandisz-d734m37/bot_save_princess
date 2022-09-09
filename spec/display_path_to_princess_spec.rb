4ce65a5b2d39dde74517bfb21242b79dd2e94570
Sandisz Thieme <sandiszthieme@gmail.com>
2022-09-08 21:17:29 -0600

    Test: create_path Write spec test to ensure create_path method returns array of directions

--- spec/display_path_to_princess_spec.rb
+++ spec/display_path_to_princess_spec.rb
@@ -14,3 +14,16 @@ describe "matrix" do
         expect(locate_princess(array_matrix)).to eq([2, 0])
     end
 end
+
+describe "path" do
+    before do
+        str = "-,-,-\n-,m,-\np,-,-"
+        @matrix = create_matrix(str)
+        @princess_loc = locate_princess(@matrix)
+        @user_loc = [1,1]
+    end
+    it "outputs an array of moves made to get the user to the princess" do
+        expect(@princess_loc).to eq([2,0])
+        expect(create_path(@user_loc, @princess_loc)).to eq(["Down","Left"])
+    end
+end
