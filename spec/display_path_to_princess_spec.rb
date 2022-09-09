f3e60f6afb179612f6ee7e588633bb8f8fd8397a
Sandisz Thieme <sandiszthieme@gmail.com>
2022-09-08 20:39:22 -0600

    Test: locate_princess Write test for the locate_princess method which intakes the matrix and returns the princesses coordinates (i.e. [2, 0] = bopttom left corner 3X3 grid)

--- spec/display_path_to_princess_spec.rb
+++ spec/display_path_to_princess_spec.rb
@@ -9,5 +9,8 @@ describe "matrix" do
         
         expect(create_matrix(@matrix)).to eq([["-","-","-",],["-","m","-",],["p","-","-",]])
     end
-
+    it "locates the princess" do
+        array_matrix = create_matrix(@matrix)
+        expect(locate_princess(array_matrix)).to eq([2, 0])
+    end
 end
