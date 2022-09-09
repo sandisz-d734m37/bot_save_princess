9f8246a59cd520abebc23e65f998dc0766063c73
Sandisz Thieme <sandiszthieme@gmail.com>
2022-09-09 16:49:37 -0600

    Refactor: create_path Refactor the create_path method to match specificities of Hackerrank i.e. instead of `puts "down"`, the output is now `print "DOWN\n"` Aditionally, instead of shovelling into an array then printing that, The method saves a step by simply printing the first time around.

--- bot_save_princess.rb
+++ bot_save_princess.rb
@@ -1,18 +1,17 @@
 require "pry"
 require_relative "old_create_matrix.rb"
 
-def displayPathtoPrincess(num, matrix)
-    matrix = create_matrix(matrix)
-    user_loc = [num/2, num/2]
-    princess_loc = locate_princess(matrix)
-    move_array = create_path(user_loc, princess_loc)
-    move_array.join("\n")
+
+def displayPathtoPrincess(n, grid)
+    grid = create_matrix(grid)
+    user_loc = [n/2, n/2]
+    princess_loc = locate_princess(grid)
+    create_path(user_loc, princess_loc)
 end
 
 def create_matrix(matrix)
-    matrix = matrix.split("\n")
     matrix.map do |string|
-        string.split(",")
+        string.split("")
     end
 end
 
@@ -23,34 +22,34 @@ def locate_princess(matrix)
             if item == "p"
                 location << row_num
                 location << column_num
+                return location
             end
         end
     end
-    return location
 end
 
 def create_path(user_loc, princess_loc)
-    moves = []
     user_loc.each_with_index do |num, index|
         until num == princess_loc[index]
             if index == 0
                 if num < princess_loc[index]
                     num += 1
-                    moves << "Down"
+                    print "DOWN\n"
                 elsif num > princess_loc[index]
                     num -= 1
-                    moves << "Up"
+                    print "UP\n"
                 end
             else
                 if num < princess_loc[index]
                     num += 1
-                    moves << "Right"
+                    print "RIGHT\n"
                 elsif num > princess_loc[index]
                     num -= 1
-                    moves << "Left"
+                    print "LEFT\n"
                 end 
             end
         end
     end
-    return moves
 end
+
+
