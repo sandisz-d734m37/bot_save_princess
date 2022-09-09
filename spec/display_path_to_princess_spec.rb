9917002502479793028ee7e5517540e762892e3a
Sandisz Thieme <sandiszthieme@gmail.com>
2022-09-08 21:30:28 -0600

    Refactor/test: displayPathtoPrincess Write test to ensure displayPathtoPrincess method intakes a string & num then returns a string of movees seperated be \n Also refactor tests for readability

--- spec/display_path_to_princess_spec.rb
+++ spec/display_path_to_princess_spec.rb
@@ -1,29 +1,30 @@
 require "./bot_save_princess.rb"
 require "pry"
 
-describe "matrix" do
+describe "Bot Saves Princess" do
     before do
         @matrix = "-,-,-\n-,m,-\np,-,-"
     end
+
     it "translates the input to an array of arrays" do
-        
         expect(create_matrix(@matrix)).to eq([["-","-","-",],["-","m","-",],["p","-","-",]])
     end
-    it "locates the princess" do
-        array_matrix = create_matrix(@matrix)
-        expect(locate_princess(array_matrix)).to eq([2, 0])
-    end
-end
 
-describe "path" do
-    before do
-        str = "-,-,-\n-,m,-\np,-,-"
-        @matrix = create_matrix(str)
-        @princess_loc = locate_princess(@matrix)
-        @user_loc = [1,1]
+    it "locates the princess" do
+        matrix_array = create_matrix(@matrix)
+        expect(locate_princess(matrix_array)).to eq([2, 0])
     end
+
     it "outputs an array of moves made to get the user to the princess" do
-        expect(@princess_loc).to eq([2,0])
-        expect(create_path(@user_loc, @princess_loc)).to eq(["Down","Left"])
+        matrix_array = create_matrix(@matrix)
+        princess_loc = locate_princess(matrix_array)
+        user_loc = [1,1]
+
+        expect(princess_loc).to eq([2,0])
+        expect(create_path(user_loc, princess_loc)).to eq(["Down","Left"])
+    end
+
+    it "ultimately outputs the directions to the princess as a string" do
+        expect(displayPathtoPrincess(3, @matrix)).to eq("Down\nLeft")
     end
 end
