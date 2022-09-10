ae6e0c4a960f8ce2327a35cf8d312b384482c170
Sandisz Thieme <sandiszthieme@gmail.com>
2022-09-10 12:26:26 -0600

    Refactor/feat: create_path/adjust_user_loc Remove unecessary math in create_path + move user_loc adjustments to a new method, adjust_user_loc which makes adjustments to the user_loc array.

--- display_path_to_princess.rb
+++ display_path_to_princess.rb
@@ -15,7 +15,7 @@ def create_matrix(matrix)
 end
 
 def locate_princess(matrix)
-    location = []
+    location =[]
     matrix.each_with_index do |array, row_num|
         array.each_with_index do |item, column_num|
             if item == "p"
@@ -30,21 +30,25 @@ end
 def create_path(user_loc, princess_loc)
     until user_loc == princess_loc
         case
-        when user_loc[0] - princess_loc[0] > 0
-            move = "UP\n"
-            user_loc[0] -= 1
-        when user_loc[0] - princess_loc[0] < 0
-            move = "DOWN\n"
-            user_loc[0] += 1
-        when user_loc[1] - princess_loc[1] > 0
-            move = "LEFT\n"
-            user_loc[1] -= 1
-        when user_loc[1] - princess_loc[1] < 0
-            move = "RIGHT\n"
-            user_loc[1] += 1
+        when user_loc[0] > princess_loc[0]; move = "UP\n"
+        when user_loc[0] < princess_loc[0]; move = "DOWN\n"
+        when user_loc[1] > princess_loc[1]; move = "LEFT\n"
+        when user_loc[1] < princess_loc[1]; move = "RIGHT\n"
+        else move = "You've found the princess!"
         end
         print move
+        user_loc = adjust_user_loc(user_loc, move)
     end
+end
+
+def adjust_user_loc(user_loc, move)
+    case
+    when move == "UP\n"; user_loc[0] -= 1
+    when move == "DOWN\n"; user_loc[0] += 1
+    when move == "LEFT\n"; user_loc[1] -= 1
+    when move == "RIGHT\n"; user_loc[1] += 1
+    end
+    user_loc
 end
         
 # -------------------------------
