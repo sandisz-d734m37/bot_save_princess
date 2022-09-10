372170dfc97831c940e092c7f10c1cbbd65b58c4
Sandisz Thieme <sandiszthieme@gmail.com>
2022-09-10 12:39:23 -0600

    Refactor: test descriptions Add method names to test descriptions for readability/usability

--- spec/display_path_to_princess_spec.rb
+++ spec/display_path_to_princess_spec.rb
@@ -6,16 +6,16 @@ describe "Bot Saves Princess" do
         @matrix = ["---","-m-","p--"]
     end
 
-    it "translates the input to an array of arrays" do
+    it "create_matrix translates the input to an array of arrays" do
         expect(create_matrix(@matrix)).to eq([["-","-","-",],["-","m","-",],["p","-","-",]])
     end
 
-    it "locates the princess" do
+    it "locate_princess locates the princess" do
         matrix_array = create_matrix(@matrix)
         expect(locate_princess(matrix_array)).to eq([2, 0])
     end
 
-    it "prints the moves made to get the user to the princess" do
+    it "create_path prints the moves made to get the user to the princess" do
         matrix_array = create_matrix(@matrix)
         princess_loc = locate_princess(matrix_array)
         user_loc = [1,1]
@@ -28,7 +28,7 @@ describe "Bot Saves Princess" do
         expect(adjust_user_loc([1,1], "UP\n")).to eq([0,1])
     end
 
-    it "ultimately outputs the directions to the princess to the terminal" do
+    it "displayPathtoPrincess ultimately outputs the directions to the princess to the terminal" do
         expect{ displayPathtoPrincess(3, @matrix) }.to output("DOWN\nLEFT\n").to_stdout
     end
 end
